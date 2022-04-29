15ed5d4f2fe469ee8f8ee42184d61b48bf9207f3
bex-developers <soporte@bex.com.pe>
2022-04-29 11:39:15 -0500

    enable system without keycloak

--- src/index.js
+++ src/index.js
@@ -95,7 +95,7 @@ app.get('/tickets/customer_project/:company_id',keycloak.protect(['customer','he
 
 app.get('/tickets/customer_company/:company_id',keycloak.protect(['customer','helpdesk']), customer_company);
 app.get('/tickets/customer_class',keycloak.protect(['customer','helpdesk', 'admin']), customer_class);
-app.get('/tickets/system/:company_id',keycloak.protect(['customer','helpdesk']), system);
+app.get('/tickets/system/:company_id', system);
 app.get('/tickets/support_project/:company_id', support_project);
 app.get('/tickets/status_ticket', status_ticket);
 
