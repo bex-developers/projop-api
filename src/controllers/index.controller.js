8304e96744f07bbc7985a800d4593ca30fe399f1
mrojas-bex <mrojas@bex.com.pe>
2023-02-21 15:26:23 -0500

    config item type

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -560,7 +560,7 @@ const getCertInstance = async (req, res, next) => {
                     conf_item_id, conf_item_name, conf_item_nr,
                     conf_item_version, conf_item_code,
                     conf_item_type_id, im_category_from_id(conf_item_type_id) as conf_item_type_name,
-                    btp_srv_environment, im_category_from_id(conf_item_type_id) as btp_srv_environment_name,
+                    btp_srv_environment, im_category_from_id(btp_srv_environment) as btp_srv_environment_name,
                     lmdb_it_admin_role, lmdb_long_sid,
                     lmdb_type, lmdb_install_number
                     FROM im_conf_items
