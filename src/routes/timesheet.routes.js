a677120304769826d3b823a56b49e75dcfc47916
lgutierrez-bex <lgutierrez@basisexperts.biz>
2025-11-19 09:22:34 -0500

    Funcion create or update timesheet list

--- src/routes/timesheet.routes.js
+++ src/routes/timesheet.routes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-const { createOrUpdateTimesheet} = require('../controllers/timesheet.controller');
+const { createOrUpdateTimesheet, getUserTicketsWithHours} = require('../controllers/timesheet.controller');
 
 router.post('/create-timesheet', createOrUpdateTimesheet);
+router.get('/user-tickets-with-hours/:userId', getUserTicketsWithHours);
 
 module.exports = router;
